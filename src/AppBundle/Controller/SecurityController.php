8244436eb213fe0a7e11d08a7298bbfec3375fc1
Ryan Weaver <ryan@thatsquality.com>
2016-02-21 16:24:15 -0500

    Login: creating login form

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function loginAction()
+    {
+        $helper = $this->get('security.authentication_utils');
+
+        return $this->render('security/login.html.twig', array(
+            // last username entered by the user (if any)
+            'last_username' => $helper->getLastUsername(),
+            // last authentication error (if any)
+            'error' => $helper->getLastAuthenticationError(),
+        ));
+    }
+
+    /**
+     * @Route("/login_check", name="security_login_check")
+     */
+    public function loginCheckAction()
+    {
+        // will never be executed
+    }
+}
