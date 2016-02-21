cfb189cc5cec2cb56db4ba107c49e44cbfa9cc99
Ryan Weaver <ryan@thatsquality.com>
2016-02-21 16:24:16 -0500

    FormLogin: Inject Container

--- src/AppBundle/Security/FormLoginAuthenticator.php
+++ src/AppBundle/Security/FormLoginAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Security;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -9,6 +10,13 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class FormLoginAuthenticator extends AbstractFormLoginAuthenticator
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function getCredentials(Request $request)
     {
         // TODO: Implement getCredentials() method.
